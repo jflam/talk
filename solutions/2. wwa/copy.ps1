cbfb4d3f8fd63e44c37349cf5decd3d28d1c2eb9
John Lam <jlam@iunknown.com>
2012-06-09 11:38:48 -0700

    removed all of the old solutions

--- solutions/2. wwa/copy.ps1
+++
@@ -1,4 +0,0 @@
-# Copy files
-
-cp "c:\talk\libs\css\*" "Editor2\css"
-cp "c:\talk\libs\js\*" "Editor2\js"
--- solutions/3. light up/copy.ps1
+++
@@ -1,4 +0,0 @@
-# Copy files
-
-cp "c:\talk\libs\css\*" "Editor3\css"
-cp "c:\talk\libs\js\*" "Editor3\js"
